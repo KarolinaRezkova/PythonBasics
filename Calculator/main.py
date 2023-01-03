d3fb920f7a3d639c772f59bcef2f0279da9c8574
karolina_rezkova <karolina.rezkova@gmail.com>
2023-01-03 11:33:18 +0100

    Lesson 3.4
    Easy calculator

---
+++ Calculator/main.py
@@ -0,0 +1,14 @@
+print("Insert first float")
+a = float(input())
+print("Insert second float")
+b = float(input())
+
+sumOfNumbers = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
+print(f"The sum of the given numbers is {sumOfNumbers}")
+print(f"The difference of the given numbers is {difference}")
+print("The product of the given numbers is " + str(product))
+print("The quotient of the given numbers is " + str(quotient))
